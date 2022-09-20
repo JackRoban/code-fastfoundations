8ed5ffee438834b8331dce1a733d16cbe12e8a5f
JackRoban <jackroban10@gmail.com>
2022-09-20 13:47:43 +0100

    updates

--- day2/writing_files.py
+++ day2/writing_files.py
@@ -23,6 +23,27 @@ def writing_to_text_files():
         print(f.readlines())
 
 
+def writing_to_text_files2():
+    with open("my_fancy_file.txt", 'w') as f:
+        f.write("1")  # no newline at the end
+        f.write("2")  # no newline at the end
+        f.write("3")  # no newline at the end
+        f.write("4\n")  # now we add a new line
+        f.write("5\n")  # again
+    with open("my_fancy_file.txt") as f:
+        print(f"{f.read() = }")
+    lines_of_text = [
+        "Knowledge is power.\n",
+        "Power to do evil... or power to do good.\n",
+        "Power itself is not evil.\n",
+        "So knowledge itself is not evil.\n",
+        "― Veronica Roth, Allegiant\n",
+    ]
+    with open("power_quote.txt", 'w') as f:
+        f.writelines(lines_of_text)
+    with open("power_quote.txt") as f:
+        print(f.readlines())
+
 def creating_and_modifying_paths():
     import pathlib
     import datetime
@@ -43,6 +64,16 @@ def creating_and_modifying_paths():
     print(f"{str(my_path.parent.absolute()):{col1}}: {my_path.parent.exists() = }")
 
 
+def creating_and_modifying_paths2():
+    import pathlib
+    my_path = pathlib.Path("day2/dir1/dir3/dir5/new_file.txt")  # a non-existent path
+    my_path.parent.mkdir()
+    print(f"*Laughter*", file=f)
+    # with my_path.open('w') as f: # FileNotFoundError
+    #     print(f"{datetime.datetime.now() = }\n", file=f) # another way to write to a file
+
+
+
 def parsing_text_files():
     with open("Homo_sapiens.GRCh38.107.abinitio.gtf") as f:
         for row in f:  # files are iterators
@@ -53,10 +84,25 @@ def parsing_text_files():
             print(cols)
 
 
+def parse_GTF():
+    with open("Homo_sapiens.GRCh38.107.abinitio.gtf") as f:
+        for row in f:
+            cols = row.strip().split("\t")
+            if row[0] != "#":
+                print(cols)
+                if col[2] =='exon':
+
+data = range(10)
+with open("file.txt", "w") as f:
+    for d in data:
+        f.write("d\n")
+
 def main():
     # writing_to_text_files()
+    # writing_to_text_files2()
     # creating_and_modifying_paths()
     # parsing_text_files()
+    parse_GTF()
     return 0
 
 

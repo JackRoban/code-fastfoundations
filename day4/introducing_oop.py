1327c41f56ac8b37ab5f28ad60f9d8c87300d299
JackRoban <jackroban10@gmail.com>
2022-09-22 11:09:48 +0100

    rectangle

--- day4/introducing_oop.py
+++ day4/introducing_oop.py
@@ -1,17 +1,44 @@
+import sys
+import os
+
+
 class Rectangle:
+    units = 'cm'  # all rectangles will have the same units
+
     def __init__(self, width, height, position=(0, 0)):
-        self.width = width
-        self.height = height
+        self.width = float(width)
+        self.height = float(height)
         self.position = position
 
+
+    def __str__(self):  # Python special methods
+        return f"I am a circle of size {self.area()}{self.units} located at {self.position}."
+
+
     def area(self):
-        pass
+        return self.height * self.width
 
     def perimeter(self):
-        pass
+        return 2 * (self.width + self.height)
 
     def diagonal(self):
-        pass
+        return (self.width ** 2 + self.height ** 2) ** 1/2
 
     def bounding_box(self):
-        pass
+        coords =(self.position[0] - self.width / 2, self.width[0] - self.width / 2,
+                 self.position[1] - self.width / 2, self.width[1] - self.width / 2)
+        return coords
+
+
+def main():
+    rectangle = Rectangle(10, 15)
+    print(rectangle.area())
+    print(rectangle.perimeter())
+    print(rectangle.diagonal())
+    print(rectangle.coords())
+
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main())

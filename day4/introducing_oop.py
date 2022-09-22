cfe756c71a05a32be7ccd2661e857aafe7966875
JackRoban <jackroban10@gmail.com>
2022-09-22 11:33:37 +0100

    rectangle

--- day4/introducing_oop.py
+++ day4/introducing_oop.py
@@ -5,14 +5,21 @@ import os
 class Rectangle:
     units = 'cm'  # all rectangles will have the same units
 
-    def __init__(self, width, height, position=(0, 0)):
+    def __init__(self, width, height, position=(0, 0), fill='white', stroke='black'):
         self.width = float(width)
         self.height = float(height)
         self.position = position
+        self.fill = fill
+        self.stroke = stroke
 
 
-    def __str__(self):  # Python special methods
-        return f"I am a circle of size {self.area()}{self.units} located at {self.position}."
+class Text:
+
+    def __int__(self, size=8, position=(0, 0), txt_colour="blue"):
+        self.size = size
+        self.position = position
+        self.txt_colour = txt_colour
+
 
 
     def area(self):
@@ -25,17 +32,24 @@ class Rectangle:
         return (self.width ** 2 + self.height ** 2) ** 1/2
 
     def bounding_box(self):
-        coords =(self.position[0] - self.width / 2, self.width[0] - self.width / 2,
-                 self.position[1] - self.width / 2, self.width[1] - self.width / 2)
-        return coords
+        return (self.position[0] - self.width / 2, self.width[0] - self.width / 2,
+                self.position[1] - self.width / 2, self.width[1] - self.width / 2)
 
+
+class Canvas:
+    units = "cm"
+
+    def __init__(self, width, height, bg_colour="blue"):
+        self.width = float(width)
+        self.height = float(height)
+        self.bg_colour = bg_colour
 
 def main():
     rectangle = Rectangle(10, 15)
     print(rectangle.area())
     print(rectangle.perimeter())
     print(rectangle.diagonal())
-    print(rectangle.coords())
+    print(rectangle.bounding_box())
 
     return os.EX_OK
 
